f07a98b49a085f3058ef4dd5e286547e9448424c
Raay473667 <hornakradek@email.cz>
2023-03-30 13:37:28 +0200

    01: XRD v1 finish

--- 01_XRD/python/01_XRD.py
+++ 01_XRD/python/01_XRD.py
@@ -6,6 +6,7 @@ from functools import partial
 from matplotlib.ticker import AutoMinorLocator 
 import math
 from PyPDF2 import PdfMerger
+import scipy.signal as signal
 
 def round_down_even(x):
     """
@@ -58,17 +59,17 @@ def plot(x01, x02, p0, bounds, y_plot):
     x1, y1 = cropData(x, y, x01-0.5, x02+0.5)
     params = fit_gauss_two(x1, y1, p0=p0, bounds=bounds)
     print(*(map(lambda y: '{:.3f}'.format(y), params)))
-    #fwhm = 2*np.sqrt(2*np.log(2))*params[1]
-    fwhm = params[1]
+    #fwhm = 2*np.sqrt(2*np.log(2))*params[1]   # Already implemented in functions
+    fwhm = params[1] 
     print(f'FWHM of {x01} = {fwhm:.4f}')
     crystal_size = 0.94 * K_alfa_1 / np.radians(fwhm) / np.cos(np.radians(params[3]/2))
-    print(f'crystal_size = {crystal_size:.2f}')
+    print(f'crystal_size = {crystal_size:.2f} A')
     y_plot = y_plot + two_gaussians(x, *params)
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize =(6.4,4))
     ax.plot(x, y, label = 'Measured Data')
-    ax.plot(x, first_gaussian(x, params[0], params[1], params[3]), label="K$_{α1}$ Gaussian")
-    ax.plot(x, second_gaussian(x, params[2], params[1], params[4]), label="K$_{α2}$ Gaussian")
-    ax.plot(x, two_gaussians(x, *params), label="K$_{α1}$ + K$_{α2}$ Gaussian")
+    ax.plot(x, first_gaussian(x, params[0], params[1], params[3]), label="K$_{α1}$ Lorentzian")
+    ax.plot(x, second_gaussian(x, params[2], params[1], params[4]), label="K$_{α2}$ Lorentzian")
+    ax.plot(x, two_gaussians(x, *params), label="K$_{α1}$ + K$_{α2}$ Lorentzian")
     ax.xaxis.set_minor_locator(AutoMinorLocator(4))
     ax.yaxis.set_minor_locator(AutoMinorLocator(5))
     ax.set_xlabel("$2θ$ [°]")
@@ -86,6 +87,10 @@ def main():
     x = data[:,0]
     global y
     y = data[:,1] - 5300
+    #background = signal.savgol_filter(y, window_length=1000, polyorder=5)
+    #p = np.polyfit(x, y, 1)
+    #background = np.polyval(p, x)
+    #y = y - background
     global K_alfa_1
     K_alfa_1 = 1.540601  # Angstrom
     global K_alfa_2
@@ -107,8 +112,8 @@ def main():
     bounds = ([0, 0, 10000, 0, 0], [np.inf, 0.2, np.inf, 100, 100])
     plot(x01, x02, p0, bounds, y_plot)
     
-    x01 = 73.98
-    x02 = 74.19
+    x01 = 74
+    x02 = 74.21
     p0 = [40000, 0.1, 20000, x01, x02]
     bounds = ([0, 0, 0, 0, 0], [np.inf, 0.2, np.inf, 100, 100])
     plot(x01, x02, p0, bounds, y_plot)
@@ -130,8 +135,8 @@ def main():
     
     fig, ax = plt.subplots()
     ax.plot(x, y, label = 'Measured Data')
-    ax.plot(x, y_plot, label="K$_{α1}$ + K$_{α2}$ Gaussian")
-    ax.xaxis.set_minor_locator(AutoMinorLocator(4))
+    ax.plot(x, y_plot, label="K$_{α1}$ + K$_{α2}$ Lorentzian")
+    ax.xaxis.set_minor_locator(AutoMinorLocator(5))
     ax.yaxis.set_minor_locator(AutoMinorLocator(5))
     ax.set_xlabel("$2θ$ [°]")
     ax.set_ylabel("Intensity [a.u.]")
@@ -141,11 +146,23 @@ def main():
     
     fig, ax = plt.subplots()
     ax.plot(x, y, label = 'Measured Data')
-    ax.xaxis.set_minor_locator(AutoMinorLocator(4))
+    ax.xaxis.set_minor_locator(AutoMinorLocator(5))
     ax.yaxis.set_minor_locator(AutoMinorLocator(5))
     ax.set_xlabel("$2θ$ [°]")
     ax.set_ylabel("Intensity [a.u.]")
     ax.tick_params(which = 'both', right=True, top=True)
+    
+    peak_positions = [39, 43.14, 54, 73.98, 89.8, 95.0]
+    peak_intensity = [1500, 45000, 95000, 38000, 21000, 2000]
+    peak_labels = ['K$_β$','(111)', '(200)', '(220)', '(311)', '(222)']
+
+    # Annotate the peaks in the plot
+    for i in range(len(peak_positions)):
+        xpeak = peak_positions[i]
+        #y = spectrum_data[x] # Assuming spectrum_data is a dictionary or list
+        y = peak_intensity[i]
+        label = peak_labels[i]
+        ax.annotate(label, xy=(xpeak, y), xytext=(xpeak, y), ha='center', va='bottom')
     ax.legend()
     plt.savefig(f'plots/xrdplot.pdf')
     #plt.show()
